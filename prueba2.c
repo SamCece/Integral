81102c3f6953675a40c479bf8017371dec427673
Kevin Islas <kevin.r-guake2@hotmail.com>
2017-02-20 10:24:17 -0600

    Modificado y agregado
    Se agrego un programa (prueba3) definitiva
    Se modifico prueba 2 durante las experimentaciones del codigo

---
+++ prueba2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+float lim_inf,lim_sup,incremento;
+float x, integral=0.0;
+float f(float x)
+{return 2*x;}
+
+int main()
+{
+  printf("Introduzca el limite superior\n");
+  scanf("%f", &lim_sup);
+  system("cls");
+  do {
+    printf("Introduzca limite inferior\n");
+    scanf("%f", &lim_inf);
+    x=lim_inf;
+  } while(lim_sup<lim_inf);
+  system("cls");
+  do {
+    printf("Introduzca el incremento\n");
+    scanf("%f", &incremento);
+  } while(incremento<=0);
+  system("cls");
+  while (x<(lim_sup-lim_inf))
+  {
+    integral+=incremento*f(x+incremento/2.0);
+    x+=incremento;
+    printf("%.3f %.3f\n", integral,x);
+  }
+  //-------------------------------------------------------------
+  //esto hace la operacion para encontrar el valor de la integral
+  integral+=(lim_sup-x)*f(lim_inf+x/2.0);
+  printf("\nvalor de la integral =%f", integral);
+return 0;
+}
---
+++ prueba3.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+int i=0;
+float liminf, limsup, inter, deltax, suma, integral;
+
+int main ()
+{
+  printf("Calculo de una integral definida\n");
+  do{
+    printf("Dame el limite superior\n");
+    scanf("%f", &limsup);
+  }while(limsup <= 0);
+  system("cls");
+
+  do{
+    printf("Dame el limite inferior\n");
+    scanf("%f", &liminf);
+  }while (liminf>limsup);
+  system("cls");
+
+  do{
+    printf("Dame el intervalo\n");
+    scanf("%f", &inter);
+  }while (inter<=0);
+  system("cls");
+
+  suma=0.0;
+  deltax = (limsup-liminf)/inter;
+  printf("%f\n", deltax);
+  for(i=0;i<=inter;i++)
+  {
+    if (i==0){
+      suma += 2*(liminf);
+      printf("%f\n", suma);
+    }
+    else if (i>0 && i<inter)
+    {
+    	suma += 2*(2*(liminf+(i*deltax)));
+    	printf("%f\n", suma);
+    }
+    else if(i==inter){
+      suma += 2*(liminf+i*deltax);
+      printf("%f\n", suma);
+    }
+  }
+  
+  integral = (deltax/2)*suma;
+  printf("El valor aprox. de la integral es: %f\n", integral);
+  return 0;
+}
